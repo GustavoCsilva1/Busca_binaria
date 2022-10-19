1486ea180eb1813b2fb89e0b882dcc23fde4e34d
Gustavo <80724237+GustavoCsilva1@users.noreply.github.com>
2022-10-19 11:00:59 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,6 +1,6 @@
 const lista = []
 
-for (let i = 1; i <= 8; i++) {
+for (let i = 1; i <= 10; i++) {
     lista.push(i)
 }
 
@@ -11,27 +11,26 @@ for (let i = 1; i <= 8; i++) {
 //     }
 // }
 
-const busca_binaria = (lista, left, right, valor)=>{  //parametros lista ordenado  valor esquerdo e valor direito, valor ha ser buscado
-    console.log('alo', left, right)  
-   if(right >= left){   // verifica se a lista esta ordenada. Se o lado direito for maior ou igual ao valor do lado esquerdo
-      indice = parseInt(left + (right-left)/2) // divide a lista em 2 ou achar o meio do lista : lista de 5 posição formula left + (right-left)/2 =   1 + (5-1/2)
+const max = 10
+const min = 0
+const busca_binaria = (lista, max, min, valor)=>{  //parametros lista ordenado  valor esquerdo e valor direito, valor ha ser buscado
 
-      if(lista[indice] === valor){ //se o valor do meio do lista for igual ao valor  desejado achamos o valor fim do programa
-        console.log('achou!')
+  console.log('alo', min, max)  
+      indice = parseInt((min + max) / 2) // divide a lista em 2 ou achar o meio do lista : lista de 5 posição formula left + (right-left)/2 =   1 + (5-1/2)
+      if(indice === valor){ //se o valor do meio do lista for igual ao valor  desejado achamos o valor fim do programa
+        console.log('achou! seu numero é: ', valor)
         return valor
+      } if(lista[indice] > valor){ //se o valor do meio do lista for maior que o valor devemos voltar uma posição do lista pois os valores menores são os do lado esquedo do lista <-
+        console.log('sobe mais')
+        return busca_binaria(lista, min, valor)
+        
+      } else 
+       return busca_binaria(lista, indice+1, max, valor) // se o valor do meio do lista for menor que o valor desejado devemos andar uma posição adiante ->
       }
-      if(lista[indice] > valor){ //se o valor do meio do lista for maior que o valor devemos voltar uma posição do lista pois os valores menores são os do lado esquedo do lista <-
-        console.log('ta na esquerda')
-        return busca_binaria(lista, left, indice-1, valor)
-      }
-      console.log('ta na direita')
-      return busca_binaria(lista, indice+1, right, valor) // se o valor do meio do lista for menor que o valor desejado devemos andar uma posição adiante ->
-     }
 
-    return "não achou"
-}
 
-const output = busca_binaria(lista, 0, lista.length-1, 8);
+const output = busca_binaria(lista, 0, lista.length, 10);
+
 
 
 
