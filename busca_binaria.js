47bea11ce4bfd51ffc0d7d296fa694d3ee68a34b
gustavo <gustavobcs1@hotmail.com>
2022-10-19 15:03:01 -0300

    meu commit

---
+++ busca_binaria.js
@@ -0,0 +1,13 @@
+const busca_binaria = ({
+    lista_ordenada, valor_para_encontrar
+}) => {
+    const primeiro_indice_da_lista = 0;
+    const ultimo_indice_da_lista = lista_ordenada.lenght-1;
+
+    const indice_do_meio = parseInt(ultimo_indice_da_lista / 2)
+
+
+}
+
+
+module.exports = {busca_binaria}
